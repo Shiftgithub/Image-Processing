f3b64dc27a5959b2faa74d08fe71ed6db4776c73
shiftgithub <mhturanmohammad@gmail.com>
2023-09-29 18:37:08 +0600

    negetive image

---
+++ negetive_image.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('images/lena.jpg')
+
+# img = cv2.imread('images/lena.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('input image', img)
+
+# Calculate the negative image by subtracting each pixel value from 255
+negative_img = 255 - img
+
+cv2.imshow('negative image', negative_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
